0b0c5679dedb255c0177d7ffeb99c1327a03fc37
suzuka1-admin <suzuka.fukami@gmail.com>
2021-06-05 16:16:28 +1000

    Modified Indexes to add Member.tsx

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { HomeScreen, LoginScreen, RegisterScreen } from "../screens";
+import { HomeScreen, LoginScreen, MemberScreen, RegisterScreen } from "../screens";
 
 export type RootStackParamList = {
   Home: undefined;
   Login: undefined;
+  Member: undefined;
   Register: undefined;
 };
 
@@ -17,6 +18,7 @@ export default function Navigation() {
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Member" component={MemberScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
       </Stack.Navigator>
     </NavigationContainer>
